3bde46d2d0e9707f645ac090fc146c86e2c2aad4
BlueGandalf <pksmith137@gmail.com>
2017-04-26 22:36:28 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public static void main(String[] args){
+    System.out.println("hello");
+    TrainRouteFinder trf = new TrainRouteFinder();
+}
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+   
+}
